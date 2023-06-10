68cbf58f6621d44b6278080933fdd95e3500de54
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2023-06-10 11:36:10 -0300

    Criando controladores dos métodos HTTP

--- controller/controller.js
+++ controller/controller.js
@@ -5,8 +5,14 @@ exports.login_get = (req, res) => {
     res.render('login')
 }
 exports.signup_post = (req, res) => {
+    const { email, password } = req.body
+    
+    console.log(email, password)
     res.send('new signup')
 }
 exports.login_post = (req, res) => {
+    const { email, password } = req.body
+    
+    console.log(email, password)
     res.send('user login')
 }
